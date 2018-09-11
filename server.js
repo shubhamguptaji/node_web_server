1290b85afd612ec50c62e5990d41991d89e94dbb
Shubham Gupta <shubham2604gupta@gmail.com>
2018-09-11 22:50:57 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const hbs = require('hbs');
+
+
+var app = express();
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render("about.hbs");
+});
+
+
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000...");
+});
